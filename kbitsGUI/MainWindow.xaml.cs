ce73199e6d7a42dfae9b9d7c192d8e404182186f
Juan Carlos Ortega Santiago <tecprogjuancarlos@gmail.com>
2019-01-17 00:56:41 -0600

    Prama para convertir de numero entero a binaro (con signo)

---
+++ kbitsGUI/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kbitsGUI
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int numero;
+        int numerodebit;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtnumBits.IsEnabled = false;
+            txtNumEntero.IsEnabled = false;
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            txtnumBits.IsEnabled = true;
+            txtNumEntero.IsEnabled = true;
+            txtNumEntero.Clear();
+            txtnumBits.Clear();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtnumBits.Text) || string.IsNullOrWhiteSpace(txtNumEntero.Text))
+            {
+                MessageBox.Show("Ingresa algun dato porfavor", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                try
+                {
+                    numero = int.Parse(txtNumEntero.Text);
+                    numerodebit = int.Parse(txtnumBits.Text);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            int numeroCopia = numero;
+            string CodigoBinario = "";
+
+
+            numero = Math.Abs(numero);
+            
+            string bits = "";
+
+            while (numero>0)
+            {
+                int resu;
+                numero =Math.DivRem(numero, 2, out resu);                
+                bits += resu;
+            }
+
+            if (numerodebit <= bits.Count())
+            {
+                MessageBox.Show("El numero de bits ingresado no es correcto", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                
+                if (numeroCopia > 0)
+                    CodigoBinario += "0";
+                else
+                    CodigoBinario += "1";
+
+                for (int i = bits.Count(); i > 0; i--)
+                    CodigoBinario += bits[i-1];
+
+            }
+
+            lblResul.Content = CodigoBinario;
+        }
+    }
+}
